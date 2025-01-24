98973a0dfb59de8b4e9eba263d5ea3c72a47002e
praveen_minion_1998 <praveen@3analytics.com>
2025-01-24 13:06:23 +0530

    added env for the dev

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,25 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { APP_GUARD } from '@nestjs/core';
-import { RolesGuard } from './guard/roles.guard';
-import { AuthModule, InspectionModule, UsersModule } from './features';
-import { JwtAuthGuard } from './features/auth/guards';
-import { PropertyModule } from './features/property/property.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
+import { APP_GUARD } from "@nestjs/core";
+import { RolesGuard } from "./guard/roles.guard";
+import { AuthModule, InspectionModule, UsersModule } from "./features";
+import { JwtAuthGuard } from "./features/auth/guards";
+import { PropertyModule } from "./features/property/property.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'postgres',
+      type: "mssql",
       host: process.env.POSTGRES_HOST,
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DATABASE,
       port: parseInt(process.env.POSTGRES_PORT),
-      logging: process.env.MODE == 'DEV',
-      entities: ['dist/entities/*.js'],
+      logging: process.env.MODE == "DEV",
+      entities: ["dist/entities/*.js"],
       //synchronize: true,
     }),
     AuthModule,
