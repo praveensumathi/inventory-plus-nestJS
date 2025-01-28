51f3d4fa18fbc867c5cf40269227930d1ce6d590
praveenR <praveen.r@deventure.co>
2025-01-28 12:00:10 +0530

    api fix

--- src/features/inspection/inspection.controller.ts
+++ src/features/inspection/inspection.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Get, Request } from '@nestjs/common';
-import { InspectionService } from './inspection.service';
-import { Roles } from 'src/decorator';
+import { Controller, Get, Request } from "@nestjs/common";
+import { InspectionService } from "./inspection.service";
+import { Roles } from "src/decorator";
+import { ApiBearerAuth } from "@nestjs/swagger";
 
-@Controller('inspection')
+@ApiBearerAuth()
+@Controller("inspection")
 export class InspectionController {
   constructor(private readonly inspectionService: InspectionService) {}
 
-  @Get('profile')
-  @Roles(['admin'])
+  @Get("profile")
   getAllInspection(@Request() req) {
     return req.user;
   }
