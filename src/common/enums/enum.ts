841b16605ad9f31f8b032e775638933f6656e0fc
praveenR <praveen.r@deventure.co>
2025-02-27 22:45:17 +0530

    added basic static data's

--- src/common/enums/enum.ts
+++ src/common/enums/enum.ts
@@ -1,10 +1,3 @@
-export enum RolesEnum {
-  Admin = 1,
-  Clerk,
-  Manager,
-  Typist,
-}
-
 export enum StatusEnum {
   Active = 1,
   InActive = 0,
@@ -20,4 +13,56 @@ export enum InspectionType {
   HMO = 7,
   SCHEDULE_CONDITION_CHECK_IN = 8,
   SCHEDULE_CONDITION_CHECK_OUT = 9,
+}
+
+export enum RolesEnum {
+  //SUPERADMIN = 1,
+  ADMIN = 1,
+  MANAGER = 2,
+  CLERK = 3,
+  TYPIST = 4,
+}
+
+export enum InspectionStatusEnum {
+  PENDING = 1,
+  ASSIGNED = 2,
+  ACTIVE = 3,
+  PROCESSING = 4,
+  REVIEW = 5,
+  COMPLETE = 6,
+  CLOSED = 7,
+}
+
+export enum InspectionStatusTypistEnum {
+  PENDING = 1,
+  IN_PROGRESS = 2,
+  ACTIVE = 3,
+  COMPLETE = 4,
+  CLOSED = 5,
+}
+
+export enum FurnishingStatusEnum {
+  FURNISHED = 1,
+  UNFURNISHED = 2,
+  PART_FURNISHED = 3,
+}
+
+export enum BuildingTypeEnum {
+  HOUSE = 1,
+  APARTMENT = 2,
+  MAISONETTE = 3,
+  TERRACE_HOUSE = 4,
+  BUNGALOW = 5,
+  TOWN_HOUSE = 6,
+  OTHER = 7,
+}
+
+export enum PersonTitleEnum {
+  MR = 1,
+  MRS = 2,
+  MISS = 3,
+  MS = 4,
+  MX = 5,
+  DR = 6,
+  LORD = 7,
 }
--- src/features/static/static.service.ts
+++ src/features/static/static.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from "@nestjs/common";
-import { InspectionType } from "src/common/enums/enum";
+import {
+  BuildingTypeEnum,
+  FurnishingStatusEnum,
+  InspectionStatusEnum,
+  InspectionStatusTypistEnum,
+  InspectionType,
+  PersonTitleEnum,
+  RolesEnum,
+} from "src/common/enums/enum";
 
 @Injectable()
 export class StaticService {
+  getStaticData() {
+    return {
+      inspectionTypes: this.getInspectionTypes(),
+      roles: this.getRoles(),
+      inspectionStatuses: this.getInspectionStatuses(),
+      inspectionStatusesForTypist: this.getInspectionStatusesForTypist(),
+      furnishingStatuses: this.getFurnishingStatuses(),
+      buildingTypes: this.getBuildingTypes(),
+    };
+  }
+
   private getInspectionTypes() {
     return [
       {
@@ -29,9 +48,87 @@ export class StaticService {
     ];
   }
 
-  getStaticData() {
-    return {
-      inspectionTypes: this.getInspectionTypes(),
-    };
+  private getRoles() {
+    return [
+      //{ value: RolesEnum.SUPERADMIN, label: "Superadmin" },
+      {
+        value: RolesEnum.ADMIN,
+        label: "Admin",
+        description:
+          "Admins have full access to all areas of the system, like managing settings, clients and other users.",
+      },
+      {
+        value: RolesEnum.MANAGER,
+        label: "Manager",
+        description:
+          "Managers have access to all properties, inspections, users, and clients.",
+      },
+      {
+        value: RolesEnum.CLERK,
+        label: "Clerk",
+        description:
+          "Standard Clerks are limited to being able to see and manage inspections assigned to them.",
+      },
+      {
+        value: RolesEnum.TYPIST,
+        label: "Typist",
+        description:
+          "Typists are limited to only being able to access and update inspections in the Processing stage",
+      },
+    ];
+  }
+
+  private getInspectionStatuses() {
+    return [
+      { value: InspectionStatusEnum.PENDING, label: "Pending" },
+      { value: InspectionStatusEnum.ASSIGNED, label: "Assigned" },
+      { value: InspectionStatusEnum.ACTIVE, label: "Active" },
+      { value: InspectionStatusEnum.PROCESSING, label: "Processing" },
+      { value: InspectionStatusEnum.REVIEW, label: "Review" },
+      { value: InspectionStatusEnum.COMPLETE, label: "Complete" },
+      { value: InspectionStatusEnum.CLOSED, label: "Closed" },
+    ];
+  }
+
+  private getInspectionStatusesForTypist() {
+    return [
+      { value: InspectionStatusTypistEnum.PENDING, label: "Pending" },
+      { value: InspectionStatusTypistEnum.IN_PROGRESS, label: "In Progress" },
+      { value: InspectionStatusTypistEnum.ACTIVE, label: "Active" },
+      { value: InspectionStatusTypistEnum.COMPLETE, label: "Complete" },
+      { value: InspectionStatusTypistEnum.CLOSED, label: "Closed" },
+    ];
+  }
+
+  private getFurnishingStatuses() {
+    return [
+      { value: FurnishingStatusEnum.FURNISHED, label: "Furnished" },
+      { value: FurnishingStatusEnum.UNFURNISHED, label: "Unfurnished" },
+      { value: FurnishingStatusEnum.PART_FURNISHED, label: "Part Furnished" },
+    ];
+  }
+
+  private getBuildingTypes() {
+    return [
+      { value: BuildingTypeEnum.HOUSE, label: "House" },
+      { value: BuildingTypeEnum.APARTMENT, label: "Apartment" },
+      { value: BuildingTypeEnum.MAISONETTE, label: "Maisonette" },
+      { value: BuildingTypeEnum.TERRACE_HOUSE, label: "Terrace House" },
+      { value: BuildingTypeEnum.BUNGALOW, label: "Bungalow" },
+      { value: BuildingTypeEnum.TOWN_HOUSE, label: "Town House" },
+      { value: BuildingTypeEnum.OTHER, label: "Other" },
+    ];
+  }
+
+  private PersonTitleEnum() {
+    return [
+      { value: PersonTitleEnum.MR, label: "Mr" },
+      { value: PersonTitleEnum.MRS, label: "Mrs" },
+      { value: PersonTitleEnum.MISS, label: "Miss" },
+      { value: PersonTitleEnum.MS, label: "Ms" },
+      { value: PersonTitleEnum.MX, label: "Mx" },
+      { value: PersonTitleEnum.DR, label: "Dr." },
+      { value: PersonTitleEnum.LORD, label: "Lord" },
+    ];
   }
 }
